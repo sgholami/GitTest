7a766da12203f0a64ee946605980126f4d9a7d54
sgholami <soogi@asu.edu>
2014-08-21 10:48:02 -0700

    second commit

--- GItTest/src/ir/sgholami/git/GitTest.java
+++ GItTest/src/ir/sgholami/git/GitTest.java
@@ -2,6 +2,6 @@ package ir.sgholami.git;
 
 public class GitTest {
 	public static void main(String[] args) {
-		System.out.println("Git is fun .. what?");
+		System.out.println("Git is fun .. what? what?");
 	}
 }
