da76e9307edcf0d62ee70b08e7fecbb77e893023
sgholami <soogi@asu.edu>
2014-09-04 12:14:48 -0700

    Reading File capability tested and validated input.txt added to the project

--- GItTest/src/ir/sgholami/git/GitTest.java
+++ GItTest/src/ir/sgholami/git/GitTest.java
@@ -5,10 +5,13 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class GitTest {
+	
+	public static String baseAddress = "/home/sgholami/Music/inputFiles/";
+	
 	public static void main(String[] args) throws FileNotFoundException {
 		System.out.println("Git is fun .. what? what?");
 		
-		File inFile = new File("input.txt");
+		File inFile = new File(GitTest.baseAddress + "input.txt");
 		Scanner reader = new Scanner(inFile);
 		
 		while(reader.hasNext())
