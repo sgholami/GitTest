053ad01e9a08730e3ca421fff21bc7328d463e22
sgholami <soogi@asu.edu>
2014-09-04 11:20:11 -0700

    Added file reading capability

--- GItTest/src/ir/sgholami/git/GitTest.java
+++ GItTest/src/ir/sgholami/git/GitTest.java
@@ -1,7 +1,19 @@
 package ir.sgholami.git;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class GitTest {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		System.out.println("Git is fun .. what? what?");
+		
+		File inFile = new File("input.txt");
+		Scanner reader = new Scanner(inFile);
+		
+		while(reader.hasNext())
+			System.out.println(reader.nextLine());
+		
+		reader.close();
 	}
 }
